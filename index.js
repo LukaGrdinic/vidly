24e9f535dd95b7977283e57a4b39d1a1620f0927
Luka <1lukagrdinic@gmail.com>
2019-01-10 22:38:23 +0100

    Enable error logging for uncaught exceptions
    Also - enable error logging for unhandled rejections

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('config');
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -11,15 +12,38 @@ const rentalsRouter = require('./routing/rentals');
 const usersRouter = require('./routing/users');
 const auth = require('./routing/auth');
 const error = require('./middleware/error');
+
+process.on('uncaughtException', ex => {
+  console.log('WE GOT AN UNCAUGHT EXCEPTION');
+  winston.log('error', ex.message); // This does not log unhandled exceptions in logfile.log
+  process.exit(1);
+});
+process.on('unhandledRejection', ex => {
+  console.log('WE GOT AN UNHANDLED REJECTION');
+  winston.log('error', ex.message);
+  process.exit(1); // This does not allow the logs to be written in logfile.log
+});
+
+winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+
 /* if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
   process.exit(1);
 } */
-winston.add(new winston.transports.File({
-  filename: 'logfile.log',
-  handleExceptions: true
-}));
 
+winston.add(
+  new winston.transports.File({
+    filename: 'logfile.log',
+    handleExceptions: true
+  })
+); // This does not work, cant start the app
+/* winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/vidly'} ); */
+
+/* throw new Error('Something failed during startup'); */
+const p = Promise.reject(new Error('Something failed miserably!'));
+p.then(() => {
+  console.log('Done');
+});
 const db = mongoose
   .connect('mongodb://localhost/vidly')
   .then(() => {
